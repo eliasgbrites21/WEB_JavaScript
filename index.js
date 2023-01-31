6c745d5aaae3b2bbfed58de5640637b68b9d5845
eliasgbrites21 <eliasgbrites@gmail.com>
2023-01-31 14:47:27 -0300

    backgroundcolor changer

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-let htmlbutton = document.getElementById("css")
+let root = document.documentElement
+let htmlbutton = document.getElementById("htmltarjet")
+let cssbutton = document.getElementById("css")
 htmlbutton.addEventListener("click", ()=> {console.log("Click en html")})
